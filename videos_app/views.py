f7d771c98d3195e7b49b70acea71a377b69734a7
Andres.14 <feliperin14@hotmail.es>
2024-01-31 18:45:58 -0500

    creacion de la app SistemaSeguidores para gestionar el sistema de seguimiento de usuarios

--- videos_app/views.py
+++ videos_app/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib import messages
 from .models import Seguidor, User
