fa5fc77fe32960d114424028976af6aa41d1d6b3
Andres.14 <feliperin14@hotmail.es>
2024-01-31 20:13:33 -0500

    se hacen cambios en interfaz de logueo, registro y feed

--- Autenticacion/forms.py
+++ Autenticacion/forms.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 class RegistroForm(UserCreationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Tu Nombre de usuario'}), label='Nombre de usuario')
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Tu Email'}), label='Correo electronico')
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Ingresa una contraseña '}), label='Contraseña')
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Repite tu contraseña'}), label='Contraseña (confirmacion)')
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Tu Nombre de usuario', 'id': 'login'}), label='Nombre de usuario')
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Tu Email', 'id': 'login'}), label='Correo electronico')
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Ingresa una contraseña', 'id': 'login'}), label='Contraseña')
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Repite tu contraseña', 'id': 'login'}), label='Contraseña (confirmacion)')
     
     class Meta:
         model = User
