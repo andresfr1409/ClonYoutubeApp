7a31a1f61a5a761cd875565dbcfad44a970d60b3
Andres.14 <feliperin14@hotmail.es>
2024-02-02 18:35:51 -0500

    se agregan los widgets a formularios de app Gestion perfiles para personalizarlos

--- GestionPerfiles/forms.py
+++ GestionPerfiles/forms.py
@@ -5,11 +5,18 @@ class UserLoginForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email']
+        
         widgets = {
-            'email': forms.EmailInput(attrs={'class': 'form-group', 'id': 'EditarEmail'}),
+            'username': forms.TextInput(attrs={'class': 'form-control custom-input', 'id': 'FormPerfil'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control custom-input', 'id': 'FormPerfil'}),
         }
 
 class UserProfileEditForm(forms.ModelForm):
     class Meta:
         model = CustomUser
         fields = ['imagen_perfil', 'banner']
+        
+        widgets = {
+            'imagen_perfil': forms.ClearableFileInput(attrs={'class': 'form-control custom-file-input', 'id': 'FormPerfil', 'accept': 'image/*'}),
+            'banner': forms.ClearableFileInput(attrs={'class': 'form-control custom-file-input','id': 'FormPerfil', 'accept': 'image/*'}),
+        }
