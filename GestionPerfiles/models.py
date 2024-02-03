620cde0a3ba2405ff7735d7480cbf0ff8d6b2b48
Andres.14 <feliperin14@hotmail.es>
2024-02-02 20:34:46 -0500

    cambios en interfaz

--- GestionPerfiles/models.py
+++ GestionPerfiles/models.py
@@ -5,6 +5,13 @@ class CustomUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     imagen_perfil = models.ImageField(upload_to='images/', blank=True, null=True)
     banner = models.ImageField(upload_to='images/', blank=True, null=True)
-
+    
     def __str__(self):
         return self.user.username
+    
+    def delete(self, using=None, keep_parents=False):
+        if self.imagen_perfil:
+            self.imagen_perfil.storage.delete(self.imagen_perfil.name)
+        if self.banner:
+            self.banner.storage.delete(self.banner.name)
+        super().delete(using=using, keep_parents=keep_parents)
