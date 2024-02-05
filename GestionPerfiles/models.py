d288273594da9557bdbbaf75a3d83938974f6912
Andres.14 <feliperin14@hotmail.es>
2024-02-05 18:25:27 -0500

    se hacen cambios en interfaz de perfil de usuario app GestionPerfiles

--- GestionPerfiles/models.py
+++ GestionPerfiles/models.py
@@ -8,10 +8,3 @@ class CustomUser(models.Model):
     
     def __str__(self):
         return self.user.username
-    
-    def delete(self, using=None, keep_parents=False):
-        if self.imagen_perfil:
-            self.imagen_perfil.storage.delete(self.imagen_perfil.name)
-        if self.banner:
-            self.banner.storage.delete(self.banner.name)
-        super().delete(using=using, keep_parents=keep_parents)
