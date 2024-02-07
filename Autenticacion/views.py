8646121fb00bdf6659ddefaa19cb7530bd6917ef
Andres.14 <feliperin14@hotmail.es>
2024-02-07 18:33:45 -0500

    creacion de vista app Autenticacion para manejar logica de formulario de ingreso de verificacion enviado al correo para cambiar contraseña delusuario

--- Autenticacion/views.py
+++ Autenticacion/views.py
@@ -36,6 +36,29 @@ def iniciar_sesion(request):
         form = LoginForm()
     return render(request, 'Autenticacion/login/inicio_sesion.html', {'form': form})
 
+'''
+def restablecer_contraseña(request):
+    User = get_user_model()
+    if request.method == 'POST':
+        username_or_email = request.POST['username_or_email']
+        new_password = request.POST['new_password']
+        confirm_password = request.POST['confirm_password']
+        try:
+            user = User.objects.get(Q(username=username_or_email) | Q(email=username_or_email))
+        except User.DoesNotExist:
+            messages.error(request, 'Usuario no encontrado, Intentalo nuevamente .')
+            return redirect('restablecer_contraseña')
+        if new_password != confirm_password:
+            messages.error(request, 'Las contraseñas no coinciden, Intentalo nuevamente.')
+            return redirect('restablecer_contraseña')
+        # Cambiar la contraseña del usuario
+        user.password = make_password(new_password)
+        user.save()
+        messages.success(request, 'La contraseña se cambió con éxito. Ahora puedes iniciar sesión con tu nueva contraseña.')
+        return redirect('restablecer_contraseña')
+    return render(request, 'Autenticacion/login/restablecer_contraseña.html')
+'''
+
 def enviar_codigo(request):
     if request.method == 'POST':
         email = request.POST.get('email')
@@ -58,3 +81,13 @@ def enviar_codigo(request):
         else:
             messages.error(request, 'El correo electronico proporcionado no esta registrado')
     return render(request, 'Autenticacion/login/enviar_codigo.html')
+
+def ingresar_codigo(request):
+    if request.method == 'POST':
+        codigo_ingresado = request.POST.get('codigo')
+        codigo_verificacion = request.session.get('codigo_verificacion', '')
+        if codigo_ingresado == codigo_verificacion:
+            return redirect('cambiar_contraseña')
+        else:
+            messages.error(request, 'El codigo ingresado no es valido')
+    return render(request, 'Autenticacion/login/ingresar_codigo.html')
